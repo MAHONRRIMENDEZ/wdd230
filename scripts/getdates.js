627977b3d0581cc6aa6a09275b86576433d0fe91
MAHONRRIMENDEZ <97697808+MAHONRRIMENDEZ@users.noreply.github.com>
2024-07-04 18:43:34 -0600

    completed

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,4 @@
+// Hamburguer menu
 const hamburguerElement = document.querySelector('#myButton');
 const navElement = document.querySelector('.menuLinks');
 
@@ -5,7 +6,7 @@ hamburguerElement.addEventListener('click', () => {
     navElement.classList.toggle('open');
     hamburguerElement.classList.toggle('open')
 })
-
+// Current year
 function getCurrentYear() {
     const currentYearElement = document.getElementById('currentyear');
     const currentYear = new Date().getFullYear();
