aa1b4310eba5c1cb40b0f60a44ef6bc59a28efcd
MAHONRRIMENDEZ <97697808+MAHONRRIMENDEZ@users.noreply.github.com>
2024-07-12 22:15:40 -0600

    completed

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -32,3 +32,17 @@ darkModeToggle.addEventListener('click', function() {
     body.classList.toggle('dark-mode');
 });
 
+const visitsDisplay = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
+
