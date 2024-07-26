df803a68b1c661ab41dab09b11b54e2f414895e1
MAHONRRIMENDEZ <97697808+MAHONRRIMENDEZ@users.noreply.github.com>
2024-07-25 23:19:20 -0600

    completed

--- scripts/links.js
+++ scripts/links.js
@@ -1,11 +1,34 @@
-const baseURL = "https://mahonrrimendez.github.io/wdd230/";
-const linksURL = "https://mahonrrimendez.github.io/wdd230/data/links.json";
+const baseURL = "https://mahonrrimendez.github.io/wdd230";
+const url = "https://mahonrrimendez.github.io/wdd230/data/links.json";
 
 
-async function getLinks() {
-    const response = await fetch(linksURL);
-    const data = await response.json();
-    console.log(data);
+
+async function getLinksData() {
+    const response = await fetch(url);
+    const data = await response.json(); 
+    //console.log(data.weeks);
+    displayLinks(data.weeks);
 }
+getLinksData();
 
-getLinks();
+const myLinks = document.querySelector("#displayLinks");
+
+function displayLinks(weeks) {
+    weeks.forEach(item => {
+    const week = document.createElement("h4");
+    week.textContent = `${item.week}`;
+
+    const linksList = document.createElement("ul");
+    item.links.forEach(link => {
+        const listItem = document.createElement("li");
+        listItem.innerHTML=`<a href="${link.url}"> - ${link.title}</a>`
+        linksList.appendChild(listItem)
+    })
+    
+    
+    
+    myLinks.appendChild(week);
+    myLinks.appendChild(linksList)
+    });
+
+}
