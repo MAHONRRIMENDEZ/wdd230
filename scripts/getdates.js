0b62c796ba2fa5367a3a021f953e7d2ccfec380a
MAHONRRIMENDEZ <97697808+MAHONRRIMENDEZ@users.noreply.github.com>
2024-07-25 19:27:02 -0600

    completed

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -48,3 +48,51 @@ numVisits++;
 
 localStorage.setItem("numVisits-ls", numVisits);
 
+
+
+// weather
+
+const myTown = document.querySelector('#town');
+const myDescription = document.querySelector('#description');
+const myTemperature = document.querySelector('#temperature');
+const myGraphic = document.querySelector('#graphic');
+
+// key
+
+const myKey = "6a11d2af03cf9064f3896b7eb74f8b76"
+const myLat = "-17.3795"
+const myLong = "-66.1619"
+//weather path
+
+const myUrl = `//api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLong}&appid=${myKey}&units=imperial`
+
+// grabing current data
+
+
+async function apiFetch() {
+    try {
+        const response = await fetch(myUrl);
+        if (response.ok) {
+        const data = await response.json();
+        //console.log(data); // testing only
+        displayResults(data); // uncomment when ready
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+// Display results
+function displayResults(data){
+    //console.log('hello')
+    myTown.innerHTML = data.name
+    myDescription.innerHTML = data.weather[0].description
+    myTemperature.innerHTML = `${data.main.temp}&deg:F`
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+    myGraphic.setAttribute('SRC', iconsrc)
+    myGraphic.setAttribute('alt', data.weather[0].description)
+}
